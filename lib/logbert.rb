2e6673701b7e5d64e4ebf5605da2f13eb9d7de3f
Brian Lauber <constructible.truth@gmail.com>
2013-06-14 22:41:52 -0400

    Initial commit.

---
+++ lib/logbert.rb
@@ -0,0 +1,80 @@
+
+module Logbert
+
+  NameSeparator = "::"
+  
+  def self.split_name(name)
+    name.split(Logbert::NameSeparator).reject{|n| n.empty?}
+  end
+  
+  def self.name_for(name_or_module)
+    name_or_module.to_s
+  end
+  
+
+  class Logger
+    
+    attr_reader :factory, :name
+    
+    def initialize(factory, name)
+      @factory = factory
+      @name = name
+    end
+    
+    def parent
+      unless @parent_defined
+        @parent = @factory.parent_for(self)
+        @parent_defined = true
+      end
+      return @parent
+    end
+    
+    def root
+      self.factory.root
+    end
+
+  end
+
+
+  class LoggerFactory
+    
+    def initialize
+      @inventory = {}
+    end
+    
+    def [](name_or_module)
+      name = Logbert.name_for(name_or_module)
+      @inventory[name] ||= Logger.new(self, name)
+    end
+    
+    def root
+      @root ||= self['']
+    end
+    
+    def parent_for(logger)
+      n = logger.name
+      unless n.empty?
+        components = Logbert.split_name(n)
+        components.pop
+        parent_name = components.join(Logbert::NameSeparator)
+        return self[parent_name]
+      end
+    end
+
+  end
+
+
+  def self.factory
+    @factory ||= LoggerFactory.new
+  end
+
+  def self.[](name)
+    self.factory[name]
+  end
+  
+  def self.root
+    self.factory.root
+  end
+
+end
+
---
+++ logbert.gemspec
@@ -0,0 +1,13 @@
+
+Gem::Specification.new do |s|
+  s.name        = 'logbert'
+  s.version     = '0.0.1'
+  s.date        = '2013-06-14'
+  s.summary     = "Logging for winners."
+  s.description = "Change your logging behaviors without mucking with your code!"
+  s.authors     = ["Brian Lauber"]
+  s.email       = 'constructible.truth@gmail.com'
+  s.files       = Dir["lib/**/*.rb"]
+  s.license     = "MIT"
+end
+

880af67a55faf50fcdb006bf2f8ecdd8cde1295d
Brian Lauber <constructible.truth@gmail.com>
2013-06-15 02:30:41 -0400

    Decided to separate log paths using '/' instead of '::'

--- lib/logbert/naming.rb
+++ lib/logbert/naming.rb
@@ -1,14 +1,18 @@
 
 module Logbert
   
-  NameSeparator = "::"
+  NameSeparator = "/"
 
   def self.split_name(name)
     name.split(Logbert::NameSeparator).reject{|n| n.empty?}
   end
 
-  def self.name_for(name_or_module)
-    name_or_module.to_s
+  def self.name_for(n)
+    if n.is_a? Module
+      n.name.gsub("::", Logbert::NameSeparator)
+    else
+      Logbert.split_name(n.to_s).join(Logbert::NameSeparator)
+    end
   end
 
 end
--- spec/naming_spec.rb
+++ spec/naming_spec.rb
@@ -2,16 +2,40 @@
 require 'logbert/naming'
 
 describe "Logbert.split_name" do
-  it "splits names using '::'" do
-    Logbert.split_name("foo::bar").should == ["foo", "bar"]
+  it "splits names using '/'" do
+    Logbert.split_name("foo/bar").should == ["foo", "bar"]
   end
   
   it "does not include any empty components" do
-    Logbert.split_name("foo::").should == ["foo"]
-    Logbert.split_name("foo::::bar").should == ["foo", "bar"]
+    Logbert.split_name("foo/").should == ["foo"]
+    Logbert.split_name("foo//bar").should == ["foo", "bar"]
   end
   
   it "returns an empty array when given an empty string" do
     Logbert.split_name("").should == []
   end
 end
+
+
+
+module Foo
+  module Bar
+    class Quux
+    end
+  end
+end
+
+
+describe "Logbert.name_for" do
+  
+  it "assumes Modules map to a hierarchical structure" do
+    Logbert.name_for(Foo::Bar::Quux).should == "Foo/Bar/Quux"
+  end
+
+  it "removes extra '/' character from Strings" do
+    Logbert.name_for("///fwu//ha/ha///").should == "fwu/ha/ha"
+  end
+
+  
+end
+

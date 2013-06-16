0b7d875fe0a05aad5cf612062ec08237bb54acc0
Brian Lauber <constructible.truth@gmail.com>
2013-06-16 00:58:22 -0400

    Added a shortcut method for creating ProcFormatter instances

--- lib/logbert/formatters.rb
+++ lib/logbert/formatters.rb
@@ -11,23 +11,28 @@ module Logbert
       end
     end
     
-    class SimpleFormatter
+    class SimpleFormatter < Formatter
       def format(msg)
         "[#{msg.time} #{msg.pid}]: #{msg.content}"
       end
     end
     
     
-    class ProcFormatter
+    class ProcFormatter < Formatter
       attr_accessor :proc
 
       def initialize(&block)
+        raise ArgumentError, "ProcFormatter must be initialized with a block" unless block_given?
         @proc = block
       end
       
       def format(msg)
         @proc.call msg
       end
+    end
+    
+    def self.fmt(&block)
+      ProcFormatter(&block)
     end
 
   end

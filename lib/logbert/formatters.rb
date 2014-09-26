f80d214dbc10b7d61a99199fd66d4669455ffab6
Alex Bezobchuk <abezobchuk@jibe.com>
2014-09-26 14:42:15 -0400

    Fixed Message exception bug

--- lib/logbert/formatters.rb
+++ lib/logbert/formatters.rb
@@ -2,36 +2,36 @@
 require 'logbert/message'
 
 module Logbert
-  
+
   module Formatters
-        
+
     class Formatter
       def format(msg)
         raise NotImplementedError
       end
     end
-    
+
     class SimpleFormatter < Formatter
       def format(msg)
         level = msg.level.to_s.upcase.ljust(8)
         output = "#{level} [time='#{msg.time}' pid='#{msg.pid}' logger='#{msg.logger}'] : #{msg.content}"
         if msg.exception
-          output = [output, "\n\nException information:\n", msg.exception.class, ": ", msg.exception.message, "\n"]
+          output = [output, "\n\nException information:\n", msg.exception[:exc_class], ": ", msg.exception[:exc_message], "\n"]
 
-          backtrace = backtrace = msg.exception.backtrace
+          backtrace = backtrace = msg.exception[:exc_backtrace]
           if backtrace
             output += [backtrace.join($/), "\n\n"]
           else
             output << "(Backtrace is unavailable)\n\n"
           end
-          
+
           output = output.join
         end
         return output
       end
     end
-    
-    
+
+
     class ProcFormatter < Formatter
       attr_accessor :proc
 
@@ -39,12 +39,12 @@ module Logbert
         raise ArgumentError, "ProcFormatter must be initialized with a block" unless block_given?
         @proc = block
       end
-      
+
       def format(msg)
         @proc.call msg
       end
     end
-    
+
     def self.fmt(&block)
       ProcFormatter.new(&block)
     end
